d498c08afc3754e93ec008391df0483979112ee5
iiffd <sml.hng@gmail.com>
2017-10-17 21:22:24 -0700

    Made shell for Player class. Got background to initialize

--- js/app.js
+++ js/app.js
@@ -24,13 +24,29 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+  constructor() {
 
+  }
+
+  update() {
+
+  }
+
+  render() {
+
+  }
+
+  handleInput() {
+
+  }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 const allEnemies = [new Enemy()];
-const player = {};
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
