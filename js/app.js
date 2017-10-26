b6413a07b70576cdd849887bb7d073efc64d2084
iiffd <sml.hng@gmail.com>
2017-10-26 14:10:47 -0700

    Refactored enemy class using new E6 class structure

--- js/app.js
+++ js/app.js
@@ -1,36 +1,25 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+class Enemy {
+  constructor() {
     this.sprite = 'images/enemy-bug.png';
-    this.x = 25;
-    this.y = 0;
-};
+    this.x = -100;
+    this.y = 143;
+  }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
-
-
+  update(dt) {
     const choice = Math.random();
 
-    if (this.x + 101 < 505) {
+    if (this.x + 101 < 605) {
       this.x+=(101*dt);
     }
+  }
 
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+
+  }
+}
+
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -81,7 +70,12 @@ class Player {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-const allEnemies = [new Enemy()];
+const allEnemies = [];
+
+let start_point = '';
+for (let i = 0; i < 8; i++) {
+  allEnemies.push(new Enemy());
+}
 const player = new Player();
 
 // This listens for key presses and sends the keys to your
